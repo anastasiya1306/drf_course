b9d880f9e3256fcb0f6814d4fab2ce5955e0702a
Anastasiya Burykh <burykh_2000@mail.ru>
2023-07-21 19:33:47 +0300

    add hw 24.1

---
+++ course/models.py
@@ -0,0 +1,30 @@
+from django.db import models
+
+from users.models import NULLABLE
+
+
+class Course(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    preview = models.ImageField(upload_to='course/', verbose_name='Превью (картинка)', **NULLABLE)
+    description = models.TextField(verbose_name='Описание')
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Курс'
+        verbose_name_plural = 'Курсы'
+
+
+class Lesson(models.Model):
+    name = models.CharField(max_length=150, verbose_name='Название')
+    description = models.TextField(verbose_name='Описание')
+    preview = models.ImageField(upload_to='lesson/', verbose_name='Превью (картинка)', **NULLABLE)
+    link_video = models.URLField(verbose_name='Ссылка на видео', **NULLABLE)
+
+    def __str__(self):
+        return self.name
+
+    class Meta:
+        verbose_name = 'Урок'
+        verbose_name_plural = 'Уроки'
