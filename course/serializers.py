42131503e15294c26167b65f2048ace36c75b5e0
Anastasiya Burykh <burykh_2000@mail.ru>
2023-08-09 23:33:20 +0300

    add hw 25.2

--- course/serializers.py
+++ course/serializers.py
@@ -1,11 +1,13 @@
 from rest_framework import serializers
 
-from course.models import Course, Lesson, Payments
+from course.models import Course, Lesson, Payments, Subscription
+from course.validators import VideoValidator
 
 
 class LessonSerializer(serializers.ModelSerializer):
     class Meta:
         model = Lesson
+        validators = [VideoValidator(field='video')]
         fields = '__all__'
 
 
@@ -28,3 +30,9 @@ class PaymentsSerializers(serializers.ModelSerializer):
     class Meta:
         model = Payments
         fields = "__all__"
+
+
+class SubscriptionSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Subscription
+        fields = '__all__'
